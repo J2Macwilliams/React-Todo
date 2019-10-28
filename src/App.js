7f42dfc20d597b075f4461802b95c5b4cc608c0a
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-28 10:41:56 -0700

    Create form. Create handleChanges function to setState.

--- src/App.js
+++ src/App.js
@@ -11,13 +11,26 @@ class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-
+  handleChanges = e => {
+    
+    this.setState({newItem:e.target.value})
+  };
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-       
+       <form>
+         <input 
+         type="text"
+         name="item"
+         id="item"
+         placeholder="Add New Item"
+         onChange={this.handleChanges}
+         />
+         <button>Add Todo</button>
+         <button>Clear Completed</button>
+       </form>
         
       </div>
     );
